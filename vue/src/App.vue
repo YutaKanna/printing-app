e7c748758d23bd5d00ba3571f2620ead915e6938
YutaKanna <kannayuta0629@gmail.com>
2023-07-06 13:45:58 +0900

    printing app

---
+++ vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <CheckPrinter />
+  </div>
+</template>
+
+<script>
+import CheckPrinter from './components/CheckPrinter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CheckPrinter,
+  },
+};
+</script>
---
+++ vue/src/components/CheckPrinter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <button @click="printCheck">小切手を印刷</button>
+    </div>
+  </template>
+  
+  <script>
+  import html2pdf from 'html2pdf.js';
+
+  export default {
+    name: 'CheckPrinter',
+    methods: {
+      printCheck() {
+        const content = `
+          <div style="width: 8.5in; height: 11in; border: 1px solid black; padding: 1in;">
+            <h1>小切手印刷アプリ</h1>
+            <h2>小切手情報</h2>
+            <p>受取人: 山田太郎</p>
+            <p>金額: 100,000円</p>
+            <p>日付: 2023年7月6日</p>
+          </div>
+        `;
+  
+        const element = document.createElement('div');
+        element.innerHTML = content;
+  
+        html2pdf().from(element).save();
+      },
+    },
+  };
+  </script>
+  
